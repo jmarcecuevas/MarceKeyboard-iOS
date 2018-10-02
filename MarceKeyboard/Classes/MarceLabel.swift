452a27b94c666e7e72c746c2bbe74a0f3ad6840b
marceCuevas <marce.cuevas16@gmail.com>
2018-10-02 14:36:16 -0300

    MarceLabel added

---
+++ MarceKeyboard/Classes/MarceLabel.swift
@@ -0,0 +1,26 @@
+//
+//  MarceKeyboard.swift
+//  MarceKeyboard
+//
+//  Created by Marcelo Cuevas on 2/10/18.
+//
+
+import UIKit
+
+public class MarceKeyboard : UILabel {
+    
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = .Repeat | .Autoreverse
+        UIView.animateWithDuration(0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+        }, completion: nil)
+    }
+    
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
+
+
+
